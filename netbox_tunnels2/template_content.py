e973075bcee4b59924769feaf0e265aa1d964e2c
Philipp Rintz <philipp.rintz@magellan-net.de>
2023-07-25 20:47:09 +0200

    Add related tunnels to side A interfaces

---
+++ netbox_tunnels2/template_content.py
@@ -0,0 +1,26 @@
+from extras.plugins import PluginTemplateExtension
+from .models import Tunnel
+from .tables import RelatedTunnelTable
+from django.contrib.contenttypes.models import ContentType
+
+
+class InterfaceTunnels(PluginTemplateExtension):
+    model = 'dcim.interface'
+
+    def full_width_page(self):
+        obj = self.context.get('object')
+        types = ContentType.objects.get_for_model(obj)
+        tunnel = Tunnel.objects.filter(
+            side_a_assigned_object_type=types,
+            side_a_assigned_object_id=obj.id,
+        )
+        tunnel_table = RelatedTunnelTable(tunnel)
+        return self.render(
+            'netbox_tunnels2/interface_extend.html',
+            extra_context={
+                'related_tunnels': tunnel_table
+            }
+        )
+
+
+template_extensions = [InterfaceTunnels]
