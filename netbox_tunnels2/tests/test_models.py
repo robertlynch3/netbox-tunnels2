50419fc8e93068459bb354d4501f6bde31fa6e4b
Philipp Rintz <philipp.rintz@magellan-net.de>
2023-08-28 12:12:55 +0200

    Exclude netbox types from tunnel validation

--- netbox_tunnels2/tests/test_models.py
+++ netbox_tunnels2/tests/test_models.py
@@ -16,7 +16,7 @@ class TunnelTestCase(TestCase):
         self.tunnelType2 = TunnelType.objects.create(name='IPSec Tunnel', slug='ipsec')
         self.ipAddressA = IPAddress.objects.create(address='1.0.0.1/32')
         self.ipAddressB = IPAddress.objects.create(address='1.0.0.2/32')
-        self.tenant = Tenant.objects.create(name='TestTenant')
+        self.tenant = Tenant.objects.create(name='TestTenant', slug='testtenant')
         site = Site.objects.create(name='test', slug='test')
         manufacturer = Manufacturer.objects.create(name='TestManufacturer', slug='testmanufacturer')
         device_role = DeviceRole.objects.create(name='Firewall', slug='firewall')
@@ -28,7 +28,7 @@ class TunnelTestCase(TestCase):
             name='test_vm',
             site=site,
         )
-        self.interface = Interface.objects.create(name='test_intf', device=self.device)
+        self.interface = Interface.objects.create(name='test_intf', device=self.device, type='virtual')
         self.vminterface = VMInterface.objects.create(name='test_vm_intf', virtual_machine=self.vm)
         self.interface.ip_addresses.add(self.ipAddressA)
         self.vminterface.ip_addresses.add(self.ipAddressB)
@@ -56,14 +56,15 @@ class TunnelTestCase(TestCase):
                                        tunnel_type=self.tunnelType1,
                                        a_pub_address=self.ipAddressA,
                                        b_pub_address=self.ipAddressB,
-                                       side_a_assigned_object=self.interface,
                                        side_a_assigned_object_type=ContentType.objects.get_by_natural_key('dcim', 'interface'),
-                                       side_b_assigned_object_id=self.vminterface.id,
+                                       side_a_assigned_object_id=self.interface.id,
                                        side_b_assigned_object_type=ContentType.objects.get_by_natural_key('virtualization', 'vminterface'),
+                                       side_b_assigned_object_id=self.vminterface.id,
                                        tenant=self.tenant
                                        )
-        tunnel.full_clean()
+        tunnel.full_clean(exclude=['side_a_assigned_object_type', 'side_b_assigned_object_type'])
         self.assertEqual(tunnel.a_pub_address, self.ipAddressA)
         self.assertEqual(tunnel.b_pub_address, self.ipAddressB)
         self.assertEqual(tunnel.tenant, self.tenant)
         self.assertEqual(tunnel.side_a_assigned_object, self.interface)
+        self.assertEqual(tunnel.side_b_assigned_object, self.vminterface)
